77542b96e074eafa87c5735fc0d73598b28883d6
fernando <fernandoapeguero@gmail.com>
2018-05-11 16:05:09 -0400

    side menu resizing with google map with no problem need to fix listview now muahahah

--- src/App.js
+++ src/App.js
@@ -6,35 +6,121 @@ import { GoogleApiWrapper } from 'google-maps-react'
 // import child component
 import MapContainer from './MapContainer'
 
+
 class App extends Component {
 
   state = {
-     containerWidth: "0"
+     containerWidth: "0" ,
+       locations: [{
+        title: "Riverside Park",
+        location: {
+            lat: 40.867153,
+            lng: -73.871381
+        }
+    },
+    {
+        title: "la marina",
+        location: {
+            lat: 40.868745,
+            lng: -73.931965
+        }
+    },
+    {
+        title: "central park",
+        location: {
+            lat: 40.782471,
+            lng: -73.966578
+        }
+    },
+    {
+        title: "Orchard Beach",
+        location: {
+            lat: 40.867144,
+            lng: -73.794647
+        }
+    },
+    {
+        title: "Micro Center",
+        location: {
+            lat: 40.924946,
+            lng: -73.856999
+        }
+    },
+    {
+        title: "Times Square",
+        location: {
+            lat: 40.758791,
+            lng: -73.985143
+        }
+    },
+    {
+        title: "Sylvia's Restaurant of Harlem ",
+        location: {
+            lat: 40.808666,
+            lng: -73.944589
+        }
+    },
+    {
+
+        title: "Microsoft Technology Center ",
+        location: {
+            lat: 40.756674,
+            lng: -73.989900
+        }
+    }, {
+        title: "La Casa del Mofongo",
+        location: {
+            lat: 40.850363,
+            lng: -73.933332
+        }
+    },
+    {
+        title: "Spa Castle ",
+        location: {
+            lat: 40.787391,
+            lng: -73.836811
+        }
+    }
+  ] ,
+  query : "" ,
+  markers: [] ,
+  mapWiddth: 100+"%"
+
   }
 
   openDrawer = () =>{
-  this.setState({containerWidth: "250px"})
+  this.setState({containerWidth: "250px" , mapWidth:70+"%"})
   }
 
   closeDrawer = () => {
 
-    this.setState({closeDrawer: "0"})
+    this.setState({mapWidth: 100 +"%" , containerWidth: 0})
   }
 
+  onQueryUpdate = (query , event) => {
+    this.setState({query: query })
+  }
   render() {
+
+
     return (
 
            <div id="side-menu">
                <div  id="container" style={{width: this.state.containerWidth , height:"100vh"}}>
                     <div className="top-bar">
-                            <h1 >Neighborhood Map <span onClick={this.closeDrawer} id="close-sidemenu">x</span></h1>
+                            <h2 >Neighborhood Map <span onClick={this.closeDrawer} id="close-sidemenu">x</span></h2>
                         </div>
                         <div id="my-side-nav" className="side-nav">
-                            <input id="search-field" type="search" placeholder="Search" className="searcher"/>
-                            <input id="filter-button" type="button" value="Filter" className="filter-button"/>
+                            <input id="search-field" type="search" value={this.state.query} onChange={(event) => this.onQueryUpdate(event.target.value)} placeholder="Search" className="searcher"/>
+                            <input id="filter-button" type="button" value="Filter" className="filter-button" />
                         </div>
-                      <div id="listview">
-                            <ul>
+                      <div >
+                            <ul id="listview" style={{marginTop: "20px"}}>
+
+                              { this.state.locations.map(element => {
+                                  return `${element.title}`
+                              }) }
+
                                 </ul>
                       </div>
 
@@ -42,7 +128,7 @@ class App extends Component {
 
             <div onClick={this.openDrawer} className="nav-opener-holder"><span id="navigation-open" >&#9776; open</span> </div>
            <div className="mapHolder">
-             <MapContainer google={this.props.google} />
+             <MapContainer markerHolder={this.state.markers} google={this.props.google}  allMarkers={this.state.locations} mapWidth={this.mapWidth}/>
              </div>
         </div>
     );
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -4,82 +4,6 @@ import ReactDOM from 'react-dom'
 export default class MapContainer extends Component {
 
 
-   state = {
-    locations: [{
-      title: "Riverside Park",
-      location: {
-          lat: 40.867153,
-          lng: -73.871381
-      }
-  },
-  {
-      title: "la marina",
-      location: {
-          lat: 40.868745,
-          lng: -73.931965
-      }
-  },
-  {
-      title: "central park",
-      location: {
-          lat: 40.782471,
-          lng: -73.966578
-      }
-  },
-  {
-      title: "Orchard Beach",
-      location: {
-          lat: 40.867144,
-          lng: -73.794647
-      }
-  },
-  {
-      title: "Micro Center",
-      location: {
-          lat: 40.924946,
-          lng: -73.856999
-      }
-  },
-  {
-      title: "Times Square",
-      location: {
-          lat: 40.758791,
-          lng: -73.985143
-      }
-  },
-  {
-      title: "Sylvia's Restaurant of Harlem ",
-      location: {
-          lat: 40.808666,
-          lng: -73.944589
-      }
-  },
-  {
-
-      title: "Microsoft Technology Center ",
-      location: {
-          lat: 40.756674,
-          lng: -73.989900
-      }
-  }, {
-      title: "La Casa del Mofongo",
-      location: {
-          lat: 40.850363,
-          lng: -73.933332
-      }
-  },
-  {
-      title: "Spa Castle ",
-      location: {
-          lat: 40.787391,
-          lng: -73.836811
-      }
-  }
-]
-   }
-
-
-
   componentDidMount() {
     this.loadMap(); // call loadMap function to load the google map
   }
@@ -101,40 +25,38 @@ export default class MapContainer extends Component {
       this.map = new maps.Map(node, mapConfig);
       let bounds = new google.maps.LatLngBounds();
 
-      this.state.locations.forEach(loc => {
+      this.props.allMarkers.forEach(loc => {
         const marker = new google.maps.Marker({
         position: loc.location,
         title: loc.title,
         map: this.map ,
         animation: google.maps.Animation.DROP
         })
-
+        this.props.markerHolder.push(marker)
         bounds.extend(loc.location);
 
-        let infoWindow = new google.maps.InfoWindow({
-          content: loc.title
-        })
-
-        let infoWIndow = new google.maps.InfoWindow({
-          content: loc.title,
-
+        let infoWin = new google.maps.InfoWindow({
+            content: loc.title
         });
 
         marker.addListener('click',function(){
-          infoWindow.open(this.map , this);
+        infoWin.open(this.map , marker);
         })
 
       })// creates a new Google map on the specified node (ref='map') with the specified configuration set above.
 
       this.map.fitBounds(bounds);
+
+      }
     }
-  }
+
 
 
   render() {
     const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
-      width: '100%', // 90vw basically means take up 90% of the width screen. px also works.
-      height: '100vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
+      width: this.props.mapWidth, // 90vw basically means take up 90% of the width screen. px also works.
+      height: '100vh',
+       // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
     }
 
     return ( // in our return function you must return a div with ref='map' and style.
